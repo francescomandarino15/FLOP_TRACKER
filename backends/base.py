7171e7dead6b38c60471861d917517954d445524
francescomandarino15 <francescomandarino81@gmail.com>
2025-11-25 17:33:07 +0100

    Update base.py

--- backends/base.py
+++ backends/base.py
@@ -6,8 +6,8 @@ class BaseBackend(ABC):
     def __init__(self, model, logger=None):
         self.model = model
         self.logger = logger
-        self.total_flops: int = 0
-        self._last_batch_flops: int = 0
+        self.total_flop: int = 0
+        self._last_batch_flop: int = 0
         self._batch_idx: int = 0
         self._epoch_idx: int = 0
 
@@ -21,11 +21,11 @@ class BaseBackend(ABC):
         """Rimuove gli hook"""
         ...
 
-    def get_total_flops(self) -> int:
-        return int(self.total_flops)
+    def get_total_flop(self) -> int:
+        return int(self.total_flop)
 
-    def get_last_batch_flops(self) -> int:
-        return int(self._last_batch_flops)
+    def get_last_batch_flop(self) -> int:
+        return int(self._last_batch_flop)
 
     def set_epoch(self, epoch: int):
         """Opzionale: permette di tracciare l'epoch corrente nei log."""
